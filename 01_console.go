df1482fe4263ec1128aa63711a5441f6606b0036
iTulinov <energiay@gmail.com>
2022-08-17 22:06:41 +0300

    testing log

--- 01_console.go
+++ 01_console.go
@@ -3,5 +3,13 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("hello")
+	num := 5
+	myFirstString := "my string"
+	mySecondString := "my second string \n"
+
+	fmt.Print(num, mySecondString, myFirstString)
+	fmt.Println()
+	fmt.Println(num, mySecondString, myFirstString)
+	fmt.Println(num, " ", mySecondString, " ", myFirstString)
+	fmt.Printf("%d %s %s", num, myFirstString, mySecondString)
 }
