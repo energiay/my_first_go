5d5160b4b914cfbc73cb9887fd5165d8105f9691
iTulinov <energiay@gmail.com>
2022-08-17 22:24:26 +0300

    rewrite :=

--- 02_console_read_arguments.go
+++ 02_console_read_arguments.go
@@ -16,4 +16,12 @@ func main() {
 
 	io.WriteString(os.Stdout, myStr)
 	io.WriteString(os.Stdout, "\n")
+
+	// вот это новость :)
+	h, k := "4", "5"
+	i, k := "2", "3"
+	io.WriteString(os.Stdout, k)
+	io.WriteString(os.Stdout, i)
+	io.WriteString(os.Stdout, h)
+	io.WriteString(os.Stdout, "\n")
 }
