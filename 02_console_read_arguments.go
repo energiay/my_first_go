292521073e158347c26a142656661e6ed757b87c
iTulinov <energiay@gmail.com>
2022-08-18 07:58:22 +0300

    output errors

--- 02_console_read_arguments.go
+++ 02_console_read_arguments.go
@@ -14,14 +14,19 @@ func main() {
 		myStr = argument[1] + " " + argument[0]
 	}
 
-	io.WriteString(os.Stdout, myStr)
-	io.WriteString(os.Stdout, "\n")
+	io.WriteString(os.Stdout, "This is standart output\n")
+	io.WriteString(os.Stderr, myStr)
+	io.WriteString(os.Stderr, "\n")
 
 	// вот это новость :)
-	h, k := "4", "5"
-	i, k := "2", "3"
-	io.WriteString(os.Stdout, k)
-	io.WriteString(os.Stdout, i)
-	io.WriteString(os.Stdout, h)
-	io.WriteString(os.Stdout, "\n")
+	//h, k := "4", "5"
+	//i, k := "2", "3"
+	//io.WriteString(os.Stdout, k)
+	//io.WriteString(os.Stdout, i)
+	//io.WriteString(os.Stdout, h)
+	//io.WriteString(os.Stdout, "\n")
 }
+
+// go run 02_console_read_arguments.go 2>/tmp/stdError
+// cat /tmp/stdError
+// go run 02_console_read_arguments.go 2>/tmp/null
