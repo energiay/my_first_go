8eb73a18c630ba9b8cefb62acd27cd64d4f663d5
iTulinov <energiay@gmail.com>
2022-08-20 09:11:41 +0300

    work with memory

---
+++ 06_iteration_array_on_memory.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"unsafe"
+)
+
+func main() {
+	array := [...]int{1, 0, -2, 3, 4}
+	pointer := &array[0]
+	fmt.Print(*pointer, " ")
+
+	memoryAddress := uintptr(unsafe.Pointer(pointer)) + unsafe.Sizeof(array[0])
+	//fmt.Println(memoryAddress)
+	//fmt.Println(uintptr(unsafe.Pointer(pointer)))
+	//fmt.Println(unsafe.Sizeof(array[0]))
+
+	for i := 0; i < len(array)-1; i++ {
+		pointer = (*int)(unsafe.Pointer(memoryAddress))
+		fmt.Print(*pointer, " ")
+		memoryAddress = uintptr(unsafe.Pointer(pointer)) + unsafe.Sizeof(array[0])
+	}
+
+	fmt.Println("")
+	pointer = (*int)(unsafe.Pointer(memoryAddress))
+	fmt.Print("One more: ", *pointer, " ")
+	memoryAddress = uintptr(unsafe.Pointer(pointer)) + unsafe.Sizeof(array[0])
+	fmt.Println("")
+}
